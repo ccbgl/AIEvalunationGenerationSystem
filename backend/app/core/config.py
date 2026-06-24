6656f2f2c8045c28d6ebb0c732d866c5737c24a3
sjl <2746252656@qq.com>
2026-06-24 23:43:28 +0800

    test

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 import os
 from dotenv import load_dotenv
-from pydantic import BaseSettings
+# 报错
+from pydantic_settings import BaseSettings
 
 load_dotenv()
 
